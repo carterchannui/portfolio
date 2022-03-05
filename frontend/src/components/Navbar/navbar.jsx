edbd7f9132b51c07cde8746bf3a1ebf6c24952dc
Carter Chan-Nui <26908609+carterchannui@users.noreply.github.com>
2022-03-04 16:08:46 -0800

    navbar skeleton

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -1,39 +1,34 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  IconButton,
-  Heading,
-  Button,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Flex, Spacer, Box, Text, Link, IconButton, Heading, Button, Menu, MenuList, MenuButton, MenuItem, theme } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons'
-import { Flex, Spacer } from '@chakra-ui/react'
 import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 
 export default function Navbar() {
     return (
-        <Flex bg='gray.700' color='white'>
-            <Box p='2' bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text'>
-                <Heading>Carter Chan-Nui</Heading>
+        //<Flex bg='gray.700' color='white'>
+        //    <Box p='2' bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text'>
+        //        <Heading>First Last</Heading>
+        //    </Box>
+        //    <Spacer/>
+        //    <Box>
+        //        <Button colorScheme='teal' mr='4'>Projects</Button>
+        //        <Button colorScheme='teal'>About</Button>
+        //    </Box>
+        //</Flex>
+        
+        <Flex bg='gray.700'>
+            <Box p='2'>
+                <Heading size='xl' color='white' >Carter Chan-Nui</Heading>
             </Box>
-            <Spacer/>
-            <Box>
-                <Button colorScheme='teal' mr='4'>Projects</Button>
-                <Button colorScheme='teal'>About</Button>
+            <Spacer />
+            <Box p='2'>
+                <Menu>
+                    <MenuButton as={IconButton} aria-label='Options' icon={<HamburgerIcon/>}/>
+                    <MenuList>
+                        <MenuItem> About </MenuItem>
+                        <MenuItem> Projects </MenuItem>
+                    </MenuList>
+                </Menu>
             </Box>
         </Flex>
     );
