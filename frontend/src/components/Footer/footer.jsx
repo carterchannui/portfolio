93e2f72436c8a586afc00ef02030d44389a570e9
Carter Chan-Nui <26908609+carterchannui@users.noreply.github.com>
2022-03-10 20:43:22 -0800

    added footer

---
+++ frontend/src/components/Footer/footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    ChakraProvider,
+    Flex,
+    Spacer,
+    Box,
+    Text,
+    IconButton,
+    Heading,
+    Button,
+    Menu,
+    MenuList,
+    MenuButton,
+    MenuItem,
+    Center,
+    Image,
+    theme
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../../ColorModeSwitcher';
+
+export default function Footer() {
+    return (
+        <Flex p={10} bg='gray.700' color='white'>
+
+            <Center w='100%'>
+                <Text size='m2'> 2022 © Carter Chan-Nui </Text>
+            </Center>
+
+        </Flex>
+    );
+}
